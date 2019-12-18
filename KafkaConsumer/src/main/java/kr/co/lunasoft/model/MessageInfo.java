dc6a16f9754047999fb02ffb887f7c9acd18f50e
딘 <jhlee@lunasoft.co.kr>
2019-12-18 17:07:46 +0900

    modify elasticsearch model

--- KafkaConsumer/src/main/java/kr/co/lunasoft/model/MessageInfo.java
+++ KafkaConsumer/src/main/java/kr/co/lunasoft/model/MessageInfo.java
@@ -7,6 +7,8 @@ public class MessageInfo {
 	private String seq;
 	private String name;
 	private String desc;
+	private GeoPoint geopoint;
+	private ChildInfo childInfoList;
 	private String created;
 	private String requestTime;
 }
